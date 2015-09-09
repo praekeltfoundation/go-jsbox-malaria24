f26571c7c4b1a5beb81ac032186d45711d718004
Bruce McIntyre <bruce.mcintyre@gmail.com>
2015-09-09 14:08:40 +0200

    Added the ona dependencies.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ go.app = function() {
     var Choice = vumigo.states.Choice;
     var ChoiceState = vumigo.states.ChoiceState;
     var EndState = vumigo.states.EndState;
+    var Ona = require('go-jsbox-ona').Ona;
 
     var GoApp = App.extend(function(self) {
         App.call(self, 'states:start');
@@ -21,6 +22,47 @@ go.app = function() {
                 }
             });
         });
+
+
+        self.states.add('states:send-report', function(name, data) {
+        // Delegation State
+        // This state sends the collected information to the Snappy Bridge API,
+        // and then reports the success back to the user.
+            return Q()
+                .then(function() {
+                    // Send response to Ona
+                    var ona_conf = self.im.config.ona;
+                    if (typeof ona_conf == 'undefined') {
+                        return self.im.log.info([
+                            "No Ona API configured.",
+                            "Not submitting data to Ona."
+                        ].join(" "));
+                    }
+                    var ona = new Ona(self.im, {
+                        auth: {
+                            username: ona_conf.username,
+                            password: ona_conf.password
+                        },
+                        url: ona_conf.url
+                    });
+                    var submission = self.create_ona_submission(data);
+                    return ona.submit({
+                        id: self.im.config.ona.id,
+                        submission: submission,
+                    });
+                })
+                .fail(function(err) {
+                    // Check for Ona response error
+                    return self.im.log.error([
+                        'Error when sending data to Ona:',
+                        JSON.stringify(err.response)].join(' '));
+                })
+                .then(function() {
+                    // Return success to user
+                    return notify_success(name);
+                });
+        });
+
 
         self.states.add('states:end', function(name) {
             return new EndState(name, {
