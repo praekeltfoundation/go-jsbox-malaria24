8a39c924ac8c2cc18f776f060d01060f8b247732
Bruce McIntyre <bruce.mcintyre@gmail.com>
2015-09-10 08:54:20 +0200

    Simpler states.

--- src/app.js
+++ src/app.js
@@ -12,20 +12,8 @@ go.app = function() {
     // var interrupt = true;
 
 
-
+    // NEW STEPS
     self.states.add('Extract_SA_ID_Info', function(name) {
-      var question = $("");
-      return new FreeText(name, {
-        question: question,
-        check: function(content) {
-          if (true) {
-            return null; // vumi expects null or undefined if check passes
-          } else {
-            return error;
-          }
-        },
-        next: 'Submit_Case'
-      });
 
     });
     self.states.add('Facility_Code_Entry', function(name) {
@@ -73,66 +61,6 @@ go.app = function() {
 
       });
     });
-    self.states.add('Invalid_Facility_Code_Entry', function(name) {
-      var question = $("Sorry, that code is not recognised.");
-      return new ChoiceState(name, {
-        question: question,
-        choices: [
-          new Choice('Facility_Code_Entry', $("Try again")),
-          new Choice('end', $("Exit"))
-        ],
-
-        next: function(choice) {
-          return choice.value;
-        }
-
-      });
-    });
-    self.states.add('Invalid_MSISDN_Entry', function(name) {
-      var question = $("Sorry, that is an invalid cellphone number.");
-      return new ChoiceState(name, {
-        question: question,
-        choices: [
-          new Choice('MSISDN_Entry', $("Try again")),
-          new Choice('end', $("Exit"))
-        ],
-
-        next: function(choice) {
-          return choice.value;
-        }
-
-      });
-    });
-    self.states.add('Invalid_No_SA_ID_Year_Entry', function(name) {
-      var question = $("Sorry, that year is invalid.");
-      return new ChoiceState(name, {
-        question: question,
-        choices: [
-          new Choice('No_SA_ID_Year_Entry', $("Try again")),
-          new Choice('end', $("Exit"))
-        ],
-
-        next: function(choice) {
-          return choice.value;
-        }
-
-      });
-    });
-    self.states.add('Invalid_SA_ID_Entry', function(name) {
-      var question = $("Sorry, that SA ID is not invalid.");
-      return new ChoiceState(name, {
-        question: question,
-        choices: [
-          new Choice('SA_ID_Entry', $("Try again")),
-          new Choice('end', $("Exit"))
-        ],
-
-        next: function(choice) {
-          return choice.value;
-        }
-
-      });
-    });
     self.states.add('Last_Name_Entry', function(name) {
       var question = $("Please enter the first name of the patient. For example: Mbe");
       var error = $('');
@@ -147,6 +75,7 @@ go.app = function() {
         },
         next: 'Patient_Abroad_Entry'
       });
+
     });
     self.states.add('Locality_Entry', function(name) {
       var question = $("Please select the locality where the patient is currently staying:");
@@ -166,7 +95,7 @@ go.app = function() {
     });
     self.states.add('MSISDN_Entry', function(name) {
       var question = $("Please enter the cell phone number of patient or next of kin.");
-      var error = $('');
+      var error = $('Sorry, that number is not valid');
       return new FreeText(name, {
         question: question,
         check: function(content) {
@@ -176,8 +105,93 @@ go.app = function() {
             return error;
           }
         },
-        next: 'Validate_MSISDN'
+        next: 'First_Name_Entry'
       });
+    });
+    self.states.add('No_SA_ID_Day_Entry', function(name) {
+
+    });
+    self.states.add('No_SA_ID_Gender_Entry', function(name) {
+      return new ChoiceState(name, {
+        question: $("Please select the patient’s gender:"),
+        choices: [
+          new Choice('1', $("Male")),
+          new Choice('2', $("Female"))
+        ],
+
+        next: 'state_submit_case'
+      });
+
+    });
+    self.states.add('No_SA_ID_Month_Entry', function(name) {
+
+    });
+    self.states.add('No_SA_ID_Year_Entry', function(name) {
+      var question = $("Please enter the year the patient was born. For example: 1982");
+      var error = $('Sorry, that year is invalid');
+      return new FreeText(name, {
+        question: question,
+        check: function(content) {
+          if (true) {
+            return null; // vumi expects null or undefined if check passes
+          } else {
+            return error;
+          }
+        },
+        next: 'No_SA_ID_Month_Entry'
+      });
+    });
+    self.states.add('Patient_Abroad_Entry', function(name) {
+      return new ChoiceState(name, {
+        question: $("Has the patient travelled outside of the country in the past 2 weeks:"),
+        choices: [
+          new Choice('1', $("Yes")),
+          new Choice('2', $("No")),
+          new Choice('2', $("Unknown"))
+        ],
+
+        next: 'Locality_Entry'
+      });
+
+    });
+    self.states.add('SA_ID_Entry', function(name) {
+      var question = $("Please enter the patient's ID number");
+      var error = $('Sorry, that SA ID is not valid');
+      return new FreeText(name, {
+        question: question,
+        check: function(content) {
+          if (true) {
+            return null; // vumi expects null or undefined if check passes
+          } else {
+            return error;
+          }
+        },
+        next: 'Submit_Case'
+      });
+
+    });
+    self.states.add('Submit_Case', function(name) {
+      return new EndState(name, {
+        text: "Thank you! Your report has been submitted.",
+        next_state: 'Facility_Code_Entry'
+      });
+
+    });
+    // END NEW STEPS
+
+
+
+
+
+
+
+
+
+    self.states.add('Locality_Entry', function(name) {
+
+    });
+    self.states.add('MSISDN_Entry', function(name) {
+
     });
     self.states.add('No_SA_ID_Day_Entry', function(name) {
       var question = $("Please enter the day the patient was born. For example: 12");
@@ -192,17 +206,6 @@ go.app = function() {
           }
         },
         next: 'No_SA_ID_Gender_Entry'
-      });
-    });
-    self.states.add('No_SA_ID_Gender_Entry', function(name) {
-      return new ChoiceState(name, {
-        question: $("Please select the patient’s gender:"),
-        choices: [
-          new Choice('1', $("Male")),
-          new Choice('2', $("Female"))
-        ],
-
-        next: 'state_submit_case'
       });
     });
     self.states.add('No_SA_ID_Month_Entry', function(name) {
@@ -233,33 +236,6 @@ go.app = function() {
           }
         },
         next: 'Invalid_No_SA_ID_Year_Entry'
-      });
-    });
-    self.states.add('Patient_Abroad_Entry', function(name) {
-      return new ChoiceState(name, {
-        question: $("Has the patient travelled outside of the country in the past 2 weeks:"),
-        choices: [
-          new Choice('1', $("Yes")),
-          new Choice('2', $("No")),
-          new Choice('2', $("Unknown"))
-        ],
-
-        next: 'Locality_Entry'
-      });
-    });
-    self.states.add('SA_ID_Entry', function(name) {
-      var question = $("Please enter the patient's ID number");
-      var error = $('');
-      return new FreeText(name, {
-        question: question,
-        check: function(content) {
-          if (true) {
-            return null; // vumi expects null or undefined if check passes
-          } else {
-            return error;
-          }
-        },
-        next: 'Validate_SA_ID'
       });
     });
     self.states.add('Submit_Case', function(name) {
