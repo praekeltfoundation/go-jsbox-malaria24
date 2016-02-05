03d3868581919be042f4cad369a4cf7d5d6ba44b
codiebeulaine <codiebeulaine@gmail.com>
2016-02-05 15:15:15 +0200

    changing tests

--- test/ona.fixtures.js
+++ test/ona.fixtures.js
@@ -40,22 +40,22 @@ onafixtures.submit.add({
     data: {
         id: "1",
         submission: {
-          facility_code: '111111',
-          reported_by: '+27123456789',
-          first_name: 'First_Name_Entry',
-          id_type: 'none',
-          last_name: 'Last_Name_Entry',
-          locality: 'Locality_Entry',
-          locality_other: 'Locality_Entry_Other',
-          msisdn: 'MSISDN_Entry',
-          date_of_birth: '1980-02-02',
-          create_date_time: '2015-01-01T00:00:00+00:00',
-          abroad: 'Patient_Abroad_Entry',
-          sa_id_number: null,
-          gender: 'female',
-          landmark: 'Landmark_Entry',
-          landmark_description: 'Landmark_Entry_Description',
-          case_number: '20150101-111111-1' }
+            "facility_code": "111111",
+            "reported_by": "+27123456789",
+            "first_name": "First_Name_Entry",
+            "id_type": "none",
+            "last_name": "Last_Name_Entry",
+            "locality": "Locality_Entry",
+            "locality_other": "Locality_Entry_Other",
+            "msisdn": "MSISDN_Entry",
+            "date_of_birth": "1980-02-02",
+            "create_date_time": "2015-01-01T00:00:00+00:00",
+            "abroad": "Patient_Abroad_Entry",
+            "sa_id_number": null,
+            "gender": "female",
+            "landmark": "Landmark_Entry",
+            "landmark_description": "Landmark_Entry_Description",
+            "case_number": "20150101-111111-1" }
     },
     response: {
         data: {
