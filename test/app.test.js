4bd036d91e60fe5fb1f3f5cc3d59e9f18bd3a5a1
sewagodimo <edneciamat@gmail.com>
2018-01-15 16:09:48 +0200

    improved the day entry state to check if the date is valid for the given month

--- test/app.test.js
+++ test/app.test.js
@@ -48,24 +48,6 @@ describe("app", function() {
             Landmark_Entry: 'Landmark_Entry',
             Landmark_Entry_Description: 'Landmark_Entry_Description'
         };
-          var completed_answers_invalid_birth_day = {
-            Facility_Code_Entry: '111111',
-            First_Name_Entry: 'First_Name_Entry',
-            ID_Type_Entry: null, // no SA ID
-            No_SA_ID_Year_Entry: '1996',
-            No_SA_ID_Month_Entry: '02',
-            No_SA_ID_Day_Entry: '29',
-            Last_Name_Entry: 'Last_Name_Entry',
-            Locality_Entry: 'Locality_Entry',
-            Locality_Entry_Other: 'Locality_Entry_Other',
-            MSISDN_Entry: 'MSISDN_Entry',
-            date_of_birth: '1996-02-29',
-            Patient_Abroad_Entry: 'Patient_Abroad_Entry',
-            SA_ID_Entry: '1996',
-            No_SA_ID_Gender_Entry: 'female',
-            Landmark_Entry: 'Landmark_Entry',
-            Landmark_Entry_Description: 'Landmark_Entry_Description'
-        };
 
         beforeEach(function() {
             return make_im()
@@ -451,15 +433,50 @@ describe("app", function() {
                 return tester
                     .setup.user.state('No_SA_ID_Day_Entry')
                     .input('32')
-                    .check.reply.content(/Sorry, that day is invalid./)
+                    .check.interaction({
+                        state: 'No_SA_ID_Day_Entry',
+                        reply: /Please enter the day the patient was born. For example: 22/
+                    })
                     .run();
             });
 
             it('should accept a valid day', function () {
                 return tester
                     .setup.user.state('No_SA_ID_Day_Entry')
+                    .setup.user.answers({
+                    No_SA_ID_Year_Entry: "2016",
+                    No_SA_ID_Month_Entry: "02"
+                  })
                     .input('12')
                     .check.reply.content(/Please select the patient's gender/)
+                    .run();
+            });
+             it('should accept a valid date of birth', function () {
+                return tester
+                    .setup.user.state('No_SA_ID_Day_Entry')
+                    .setup.user.answers({
+                    No_SA_ID_Year_Entry: "2016",
+                    No_SA_ID_Month_Entry: "02"
+                  })
+                    .input('29')
+                    .check.interaction({
+                        state: 'No_SA_ID_Gender_Entry',
+                        reply: /Please select the patient's gender/
+                    })
+                    .run();
+            });
+            it('should not accept an invalid date of birth', function () {
+                return tester
+                    .setup.user.state('No_SA_ID_Day_Entry')
+                    .setup.user.answers({
+                    No_SA_ID_Year_Entry: "2017",
+                    No_SA_ID_Month_Entry: "04"
+                  })
+                    .input('31')
+                    .check.interaction({
+                        state: 'No_SA_ID_Year_Entry',
+                        reply: /Please enter the year the patient was born. For example: 1982/
+                    })
                     .run();
             });
         });
@@ -559,45 +576,8 @@ describe("app", function() {
                       })
                       .run();
             });
-            //test the wrong dates
-             it('should not make a valid Ona API call with an invalid date of birth', function () {
-                return tester
-                    .setup.user.state('Submit_Case')
-                    .setup.user.answers(completed_answers_invalid_birth_day)
-                    .input('1')
-                    .check.interaction({
-                        state: 'End',
-                        reply: /Thank you! Your report has been submitted\./
-                    })
-                    .check(function (api) {
-                        var http_sent = _.where(api.http.requests, {
-                            url: 'http://ona.io/api/v1/submissions'
-                        })[0];
-                        assert.deepEqual(http_sent.data, {
-                            id: '1',
-                            submission: {
-                                facility_code: '111111',
-                                reported_by: '+27123456789',
-                                first_name: 'First_Name_Entry',
-                                id_type: 'none',
-                                last_name: 'Last_Name_Entry',
-                                locality: 'Locality_Entry',
-                                locality_other: 'Locality_Entry_Other',
-                                msisdn: 'MSISDN_Entry',
-                                date_of_birth: '1995-02-29',
-                                create_date_time: '2015-01-01T00:00:00+00:00',
-                                abroad: 'Patient_Abroad_Entry',
-                                sa_id_number: null,
-                                gender: 'female',
-                                landmark: 'Landmark_Entry',
-                                landmark_description: 'Landmark_Entry_Description',
-                                case_number: '20150101-111111-1'
-                            }
-                          });
-                      })
-                      .run();
-            });
-        });
 
+
+     });
     });
 });
